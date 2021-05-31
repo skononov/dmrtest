602a8c84d7266931f37b52ee6f23132e8e2b8efb
Sergey Kononov <S.A.Kononov@inp.nsk.su>
2021-05-31 15:59:18 +0700

    testpll.c fixed with realistic frequency values

--- testpll.c
+++ testpll.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdint.h>
 
-extern void getpllreg(unsigned int setfreq, int maino, int auxo, int mltd, int mainpow, int auxpow, uint32_t R[]);
+typedef unsigned int uint;
+
+extern void getpllreg(uint setfreq, int maino, int auxo, int mltd, int mainpow, int auxpow, uint32_t R[]);
 
 int main()
 {
-	unsigned int freqs[5] = {444357, 313567, 184566, 330778, 352783};
+	const uint minfreq = 137000000, maxfreq = 500000000;
+	uint npoints = 10;
+	uint freq;
 	uint32_t R[6];
-	for(int i=0; i<5; i++) {
-        printf("Call getpllreg for frequency %u\n", freqs[i]);
-		getpllreg(freqs[i], 1, 1, 0, 0, 0, R);
+	for(int i=0; i<npoints; i++) {
+		freq = (uint)(minfreq + (double)(maxfreq-minfreq)/(npoints-1)*i);
+        printf("Call getpllreg for frequency %u\n", freq);
+		getpllreg(freq, 1, 1, 0, 0, 0, R);
 		printf("Registers: [%u, %u, %u, %u, %u, %u]\n", R[0], R[1], R[2], R[3], R[4], R[5]);
 	}
 	return 0;
