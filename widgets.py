d7bc6b7d1e252a7193d4bd3c5fb331852af19a92
Sergey Kononov <S.A.Kononov@inp.nsk.su>
2021-06-04 20:09:17 +0700

    Iterative commit of widgets.py

--- widgets.py
+++ widgets.py
@@ -10,8 +10,9 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 from config import DTConfiguration
-from tasks import dtTaskHandlers
+from tasks import dtTaskTypes, dtTaskInit
 from singleton import Singleton
+from dtglobals import DEBUG, LANG
 #import dialogs as dlg
 
 
@@ -22,7 +23,7 @@ mpl.rcParams["grid.linewidth"] = 0.5
 mpl.rcParams["axes.linewidth"] = 1.0
 mpl.rcParams["font.size"] = 12
 
-_rootWindowGeometry = "640x480+400+320"
+_rootWindowGeometry = "800x600+400+320"
 _dialogWindowGeometry = "+400+320"
 LEFT_PANEL_WIDTH = 400
 
@@ -47,26 +48,24 @@ class DTApplication(tk.Tk, metaclass=Singleton):
         self.geometry(_rootWindowGeometry)
         self.title(DTConfiguration.__appname__+' '+ DTConfiguration.__version__)
 
+        # init task handlers
+        dtTaskInit()
+
         # set styles
         self.defaultStyle()
         if os.access(DTApplication.__dtTkOptionFilename, os.R_OK):
             self.readStyle(DTApplication.__dtTkOptionFilename)
 
         self.mainMenuFrame = DTMainMenuFrame(self)
-        self.measurementFrame = DTMeasurementFrame(self)
 
-        self.mode = 'main'
-        self.render()
+        self.render(self.mainMenuFrame)
 
-    def render(self):
+    def render(self, frame: tk.Frame):
+        if frame.winfo_ismapped():
+            return
         for child in self.winfo_children():
             child.forget()
-        if self.mode == 'main':
-            self.mainMenuFrame.pack(fill=tk.BOTH)
-        elif self.mode == 'meas':
-            self.measurementFrame.pack(fill=tk.BOTH)
-        else:
-            tkmsg.showerror("Error", "Unknown window mode!")
+        frame.pack(fill=tk.BOTH)
 
     def readStyle(self, filename: str):
         self.option_clear()
@@ -107,39 +106,6 @@ class DTApplication(tk.Tk, metaclass=Singleton):
     def stop(self):
         self.destroy()
 
-class DTLogoFrame(tk.Frame, metaclass=Singleton):
-
-    def __init__(self, master, text=None, logofilename=None):
-        super().__init__(master, class_='DTLogoFrame')
-        self.configure(padx=10, pady=10, relief=tk.GROOVE)
-        
-        if text is None:
-            text = f"""
-            Информация о приложении {DTConfiguration.__appname__} {DTConfiguration.__version__}.
-            Информация о приложении {DTConfiguration.__appname__} {DTConfiguration.__version__}.
-            Информация о приложении {DTConfiguration.__appname__} {DTConfiguration.__version__}.
-            """
-        if logofilename is None and os.access('img/logo.gif', os.R_OK):
-            logofilename = 'img/logo.gif'
-        self.image = tk.PhotoImage(file=logofilename)
-
-        logo = tk.Label(self, image=self.image, padx=5, pady=5)
-        logo.pack(side=tk.TOP)
-
-        # add a text Frame
-        textboxFrame = tk.Frame(self, borderwidth=10)
-        textboxFrame.pack(side=tk.BOTTOM, fill=tk.BOTH)
-        textbox = tk.Text(textboxFrame, padx="2m", pady="1m", wrap=tk.WORD)
-
-        # add a vertical scrollbar to the frame
-        ##rightScrollbar = tk.Scrollbar(textboxFrame, orient=tk.VERTICAL, command=textbox.yview)
-        ##textbox.configure(yscrollcommand = rightScrollbar.set)
-        ##rightScrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        textbox.pack(side=tk.TOP, fill=tk.BOTH)
-        textbox.insert(tk.END, text, "normal")
-        textbox.configure(state=tk.DISABLED)
-
 
 class DTPlotFrame(tk.Frame, metaclass=Singleton):
     def __init__(self, master):
@@ -176,34 +142,67 @@ class DTMainMenuFrame(tk.Frame, metaclass=Singleton):
         super().__init__(master, class_='DTMainMenuWindow')
         self.configure(padx=10, pady=10)
 
+        self.makeLogoFrame()
+        self.logoFrame.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH)
+
         self.menuFrame = tk.Frame(self, padx=5)
         self.menuFrame.pack(side=tk.RIGHT, fill=tk.Y)
-
-        self.logoFrame = DTLogoFrame(self)
-        self.logoFrame.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH)
 
         for i in range(3):
             self.menuFrame.rowconfigure(i, minsize=50, pad=10)
 
-        csmb = self.chooseScenarioMB = tk.Menubutton(master=self.menuFrame, text='Запустить сценарий')
+        csmb = self.chooseScenarioMB = tk.Menubutton(self.menuFrame, text='Запустить сценарий')
         csmb.configure(relief=tk.RAISED, width=23, height=2, padx=5, pady=0, takefocus=tk.YES)
         csmb.grid(row=0)
         csmb.menu = DTChooseScenarioMenu(self.chooseScenarioMB)
         csmb['menu'] = csmb.menu
-        csmb.focus_set()
+        csmb.focus()
 
-        cmmb = self.chooseMeasurementMB = tk.Menubutton(master=self.menuFrame, text='Запустить измерение')
+        cmmb = self.chooseTaskMB = tk.Menubutton(self.menuFrame, text='Выбрать измерение')
         cmmb.configure(relief=tk.RAISED, width=23, height=2, padx=5, pady=0, takefocus=tk.YES)
-        cmmb.grid(row=1)
-        cmmb.menu = DTChooseMeasurementMenu(self.chooseMeasurementMB)
+        cmmb.menu = DTChooseTaskMenu(cmmb, self.runTask)
         cmmb['menu'] = cmmb.menu
+        cmmb.grid(row=1)
 
-        csb = self.newScenarioMB = tk.Button(master=self.menuFrame, text='Создать сценарий', command=self.newScenario)
+        csb = self.newScenarioMB = tk.Button(self.menuFrame, text='Создать сценарий', command=self.newScenario)
         csb.configure(relief=tk.RAISED, width=23, height=2, padx=5, pady=0)
         csb.grid(row=2)
 
     def newScenario(self):
         pass
+
+    def runTask(self):
+        pass
+
+    def makeLogoFrame(self):
+        lf = self.logoFrame = tk.Frame(self)
+        lf.configure(padx=10, pady=10, relief=tk.GROOVE)
+        
+        text = f"""
+            Информация о приложении {DTConfiguration.__appname__} {DTConfiguration.__version__}.
+            Информация о приложении {DTConfiguration.__appname__} {DTConfiguration.__version__}.
+            Информация о приложении {DTConfiguration.__appname__} {DTConfiguration.__version__}.
+            """
+        if os.access('img/logo.gif', os.R_OK):
+            logofilename = 'img/logo.gif'
+        lf.image = tk.PhotoImage(file=logofilename)
+
+        tk.Label(lf, image=self.image, padx=5, pady=5).pack(side=tk.TOP)
+
+        # add a text Frame
+        textboxFrame = tk.Frame(lf, borderwidth=10)
+        textboxFrame.pack(side=tk.BOTTOM, fill=tk.BOTH)
+        textbox = tk.Text(textboxFrame, padx="2m", pady="1m", wrap=tk.WORD)
+
+        # add a vertical scrollbar to the frame
+        ##rightScrollbar = tk.Scrollbar(textboxFrame, orient=tk.VERTICAL, command=textbox.yview)
+        ##textbox.configure(yscrollcommand = rightScrollbar.set)
+        ##rightScrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        textbox.pack(side=tk.TOP, fill=tk.BOTH)
+        textbox.insert(tk.END, text, "normal")
+        textbox.configure(state=tk.DISABLED)
+
 
 class DTNewScenarioDialog(tk.Tk):
 
@@ -218,22 +217,35 @@ class DTNewScenarioDialog(tk.Tk):
         tk.Label(frame, text='Название сценария:').grid(column=0, row=0, sticky=tk.E)
 
         self.namevar = tk.StringVar()
-        nameentry = tk.Entry(frame, textvariable=self.namevar)
-        nameentry.grid(column=1, row=0, sticky=tk.W+tk.E)
+        self.namevar.set('Новый сценарий')
+        nameentry = tk.Entry(frame, textvariable=self.namevar, width=40)
+        nameentry.grid(column=1, row=0, sticky=tk.W)
         nameentry.focus()
 
-        self.tasklist = tk.Listbox(frame, height=7)
+        tk.Label(frame, text='Задачи:').grid(column=0, row=1, sticky=tk.NE)
+
+        self.tasklist = tk.Listbox(frame, height=7, width=40)
+        self.tasklist.grid(column=1, row=1, sticky=tk.NW)
+
+        addbtn = tk.Menubutton(frame, text='Добавить задачу')
+        addbtn.menu = DTChooseTaskMenu(addbtn, command=self.addTask)
+        addbtn['menu'] = addbtn.menu
+        addbtn.grid(column=2, row=2, sticky=tk.N)
+
+        tk.Button(frame, text='Создать', command=self.create).grid(column=1, row=2, sticky=tk.S)
+    
+    def create(self):
+        pass
+
+    def addTask(self):
+        self.tasklist.insert(i, task.name[LANG])
+
+
         
-        
 
-
-
-
-class DTMeasurementFrame(tk.Frame, metaclass=Singleton):
-    pass
-    """
-    def __init__(self, master):
-        super().__init__(master, class_='DTMeasurementWindow')
+class DTTaskFrame(tk.Frame):
+    def __init__(self, master, task: DTTask):
+        super().__init__(master, class_='DTTaskFrame')
         self.configure(padx=10, pady=10)
 
         self.leftFrame = tk.Frame(self)
@@ -273,7 +285,6 @@ class DTMeasurementFrame(tk.Frame, metaclass=Singleton):
 
     def update(self):
         pass
-    """
 
 class DTChooseScenarioMenu(tk.Menu):
 
@@ -286,18 +297,17 @@ class DTChooseScenarioMenu(tk.Menu):
         for name, tasks in self.scenarios.items():
             scenarioTasksMenu = tk.Menu(self)
             for task in tasks:
-                scenarioTasksMenu.add_command(label=task, command=dtTaskHandlers[task])
+                scenarioTasksMenu.add_command(label=task, command=dtTaskTypes[task])
             self.add_cascade(label=name, menu=scenarioTasksMenu)
 
     def update(self):
         pass
 
 
-class DTChooseMeasurementMenu(tk.Menu):
-
-    def __init__(self, menubutton):
+class DTChooseTaskMenu(tk.Menu):
+    def __init__(self, menubutton, command):
         super().__init__(menubutton, tearoff = 0)
 
-        for task, handler in dtTaskHandlers.items():
-            self.add_command(label=task, command=handler)
+        for tasktype in dtTaskTypes:
+            self.add_command(label=tasktype.name[LANG], command=command)
 
