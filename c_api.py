ca00f7391bb28a299f84092f55c4eb612dcd105e
Sergey Kononov <S.A.Kononov@inp.nsk.su>
2021-06-07 01:04:30 +0700

    Test FFT analysis with blackman window.

--- c_api.py
+++ c_api.py
@@ -2,6 +2,7 @@ from ctypes import cdll, c_double, c_uint, c_int, byref
 from numpy import pi, linspace, sin, abs, sqrt
 from numpy.random import default_rng
 from scipy.special import jn
+from scipy.signal import blackman
 from scipy.fft import rfft, fftfreq
 from time import time
 import matplotlib.pyplot as plt
@@ -64,12 +65,17 @@ if __name__ == "__main__":
     T = 1/120000.  # sampling period in s
     fm = 10000  # modulating frequency
     d = 2  # modulation index
+    rmsnoise = 0.001 # rms of noise added
     t = linspace(0, T*N, N, endpoint=False)
     at = sum([jn(n, d)/jn(1, d) * sin(2*pi*(n*fm*t + rng.random()))
-              for n in range(1, 8)]) + rng.normal(0, 0.01, N)
+              for n in range(1, 10)]) + rng.normal(0, rmsnoise, N)
+
+    at2 = blackman(N)*sum([jn(n, d)/jn(1, d) * sin(2*pi*(n*fm*t + rng.random()))
+                           for n in range(1, 10)]) + rng.normal(0, rmsnoise, N)
 
     f = fftfreq(N, T)[:N//2]
     af = 2/N*abs(rfft(at))[:-1]
+    af2 = 2/N*abs(rfft(at2))[:-1]
 
     dt = timeit.timeit('2/N*abs(rfft(at))[:-1]',
                         number=100, globals=globals())/100
@@ -80,29 +86,46 @@ if __name__ == "__main__":
     print(f'get_peak execution time: {dt:3f} sec')
 
     pwr, fpeak = get_peak(af, int(fm*N*T*0.9), int(fm*N*T*1.1))
+    pwr2, fpeak2 = get_peak(af2, int(fm*N*T*0.9), int(fm*N*T*1.1))
     fpeak /= N*T  # transform to Hz
-    print(f"Power={pwr:4g}, Fpeak={fpeak:.1f}Hz")
+    fpeak2 /= N*T  # transform to Hz
+    pwr2 /= sum(blackman(N)**2)/N
+    print(f"Raw signal: Power={pwr:4g}, Fpeak={fpeak:.1f}Hz")
+    print(f"Signal w. Blackman: Power={pwr2:4g}, Fpeak={fpeak2:.1f}Hz")
 
     dt = timeit.timeit('get_inl(af, fm*N*T)',
                         number=100, globals=globals())/100
     print(f'get_inl execution time: {dt:3g} sec')
 
     inl, mi = get_inl(af, fm*N*T)
-    print(f"INL={inl:5g}, MI={mi:.2f}")
+    inl2, mi2 = get_inl(af2, fm*N*T)
+    print(f"Raw signal: INL={inl:5g}, MI={mi:.2f}")
+    print(f"Signal w. Blackman: INL={inl2:5g}, MI={mi2:.2f}")
 
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(12, 10))
     plt.subplot(211)
-    plt.plot(t[:500], at[:500], '-')
+    plt.plot(t, at, '-', label='Raw signal')
+    plt.plot(t, at2, '-', label='Signal w. Blackman window')
     plt.xlabel('Time, s')
     plt.ylabel('Amplitude')
-    plt.title(r'$f_{mod}$=%.1f kHz, MI=%.1f' % (fm/1000, d))
+    plt.legend()
+    plt.title(r'$f_{mod}$=%.1f kHz, MI=%.1f, $RMS_{noise}=%2g$' % (fm/1000, d, rmsnoise))
 
     plt.subplot(212)
-    plt.plot(f, af, '-')
+    plt.plot(f, af, 'k-', label='Raw signal FFT')
+    plt.plot(f, af2, 'b-', label='FFT of Singal w. Blackman window')
     plt.plot(fpeak, sqrt(pwr), 'ro')
     plt.xlabel('Frequency, Hz')
     plt.ylabel('Fourier amplitude')
-    plt.text(0.7*0.5/T, 0.5, 'Evaluated:\nPWR=%4g $f_{peak}$=%.1f\nINL=%4g MI=%.2f' % (pwr, fpeak, inl, mi), va='top')
     plt.semilogy()
+    ymin, ymax = plt.ylim()
+    plt.ylim(ymin, 10*ymax)
+    plt.legend()
+    plt.text(0.2*0.5/T, ymax*5, 
+             'Eval. Raw: PWR=%4g $f_{peak}$=%.1f INL=%4g MI=%.2f' % (pwr, fpeak, inl, mi) + 
+             '\nEval. Blackman: PWR=%4g $f_{peak}$=%.1f INL=%4g MI=%.2f' % (pwr2, fpeak2, inl2, mi2), 
+             va='top', ha='left')
+
+    plt.tight_layout()
     plt.savefig('../pics/fft_analysis.png')
     plt.show()
