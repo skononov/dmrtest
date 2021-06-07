a1b8cd18bfbbc399b87866c7212b2d0a102a91cb
Sergey Kononov <S.A.Kononov@inp.nsk.su>
2021-06-08 03:12:53 +0700

    testpll.c prints register values for a given frequency in command line

--- testpll.c
+++ testpll.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 typedef unsigned int uint;
 
 extern void getpllreg(uint setfreq, int maino, int auxo, int mltd, int mainpow, int auxpow, uint32_t R[]);
 
-int main()
+int main(int argc, char *argv[])
 {
-	const uint minfreq = 137000000, maxfreq = 500000000;
-	uint npoints = 10;
-	uint freq;
-	uint32_t R[6];
-	for(int i=0; i<npoints; i++) {
-		freq = (uint)(minfreq + (double)(maxfreq-minfreq)/(npoints-1)*i);
-        printf("Call getpllreg for frequency %u\n", freq);
-		getpllreg(freq, 1, 1, 0, 0, 0, R);
-		printf("Registers: [%u, %u, %u, %u, %u, %u]\n", R[0], R[1], R[2], R[3], R[4], R[5]);
+	static const uint minfreq = 137000000, maxfreq = 800000000;
+	if (argc < 2) {
+		printf("Usage: %s frequency [frequency...]\n\n\tfrequency - in MHz from 137 to 800\n", argv[0]);
+		return 0;
 	}
+
+	for(int i=1; i<argc; i++) {
+		uint freq = (uint)(atof(argv[i])*1e6);
+		if (freq < minfreq || freq > maxfreq) {
+			fprintf(stderr, "Given frequency %s is out of range [137, 800] MHz\n", argv[i]);
+			continue;
+		}
+
+		uint32_t R[6];
+		printf("Call getpllreg for frequency %u\n", freq);
+		getpllreg(freq, 1, 1, 0, 0, 0, R);
+		printf("Registers: %u %u %u %u %u %u\n", R[0], R[1], R[2], R[3], R[4], R[5]);
+	}
+	
 	return 0;
 }
