4fe55639dec8638f9f69bc5772ed74cfacbec565
Sergey Kononov <S.A.Kononov@inp.nsk.su>
2021-09-02 17:21:51 +0700

    Upper limit for PLL frequency calculation is increased in 2 times

--- dtpll.h
+++ dtpll.h
@@ -4,7 +4,7 @@
 # include <stdint.h>
 
 # define DTFREQLOWLIM 120000000
-# define DTFREQUPLIM 800000000
+# define DTFREQUPLIM 1600000000
 
 extern int getpllreg(unsigned int setfreq, int maino, int auxo, int mltd, int mainpow, int auxpow, uint32_t R[]);
 
